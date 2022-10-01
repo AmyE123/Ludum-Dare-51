50ee0a123390bf6aae69ae3468557b8a49e32cab
AmyE123 <amy.elliott2002@yahoo.co.uk>
2022-10-01 20:37:13 +0100

    Very very messy post processing manipulation code

--- Assets/Scripts/ThirdPersonMovement/Player.cs
+++ Assets/Scripts/ThirdPersonMovement/Player.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ThirdPersonMovement;
+using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering.Universal;
+using Vignette = UnityEngine.Rendering.Universal.Vignette;
+using ChromaticAberration = UnityEngine.Rendering.Universal.ChromaticAberration;
 
 [RequireComponent(typeof(PersonMovement))]
 public class Player : MonoBehaviour
@@ -17,12 +23,31 @@ public class Player : MonoBehaviour
     [SerializeField]
     private bool _hasHitWater;
 
+    #region TEMP
+    [SerializeField]
+    private GameObject _tempUI;
+
+    [SerializeField]
+    private Volume _tempPostProcessingVolume;
+    [SerializeField]
+    private Vignette _vg;
+    [SerializeField]
+    private ColorAdjustments _ca;
+    [SerializeField]
+    private ChromaticAberration _chrA;
+    #endregion
+
     PersonMovement _movement;
     CameraFollow _cameraFollow;
 
     private void Start()
     {
         InitializeValues();
+        _tempUI.SetActive(false);
+
+        _tempPostProcessingVolume.profile.TryGet(out _vg);
+        _tempPostProcessingVolume.profile.TryGet(out _ca);
+        _tempPostProcessingVolume.profile.TryGet(out _chrA);
     }
 
     private void Update()
@@ -56,7 +81,16 @@ public class Player : MonoBehaviour
     {
         if (_hasHitWater)
         {
-            _timeUntilKill -= Time.deltaTime;
+            _timeUntilKill -= Time.deltaTime;      
+            
+            _chrA.intensity.value += Time.deltaTime;
+            _ca.saturation.value -= Time.deltaTime * 30;
+
+            if (_vg.intensity.value <= 0.3f)
+            {
+                _vg.intensity.value += Time.deltaTime / 30;
+            }
+
             if (_timeUntilKill <= 0)
             {               
                 KillPlayer();
@@ -69,6 +103,7 @@ public class Player : MonoBehaviour
     private void KillPlayer()
     {
         Debug.Log("Die");
+        _tempUI.SetActive(true);
     }
 
     void HandlePlayerInput()
@@ -87,5 +122,15 @@ public class Player : MonoBehaviour
 
         _movement.ControlsInput(xComponent + yComponent);
         _movement.SetJumpRequested(Input.GetButtonDown("Jump"));
+    }
+
+    public void TEMP_RetryBTN()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void TEMP_HomeBTN()
+    {
+        SceneManager.LoadScene("TitleScene");
     }
 }
