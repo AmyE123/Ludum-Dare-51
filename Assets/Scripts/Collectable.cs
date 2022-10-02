87df7ab195e2f26673cbf1308c3c7a277f3f2faf
JoePC <iam3djoe@gmail.com>
2022-10-03 00:47:48 +0100

    added model for collectible

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -5,8 +5,30 @@ public class Collectable : MonoBehaviour
 {
     private const string PLAYER_TAG = "Player";
 
+    [SerializeField]
+    private float _spinSpeed;
+
+    [SerializeField]
+    private Transform _childTransform;
+
+    [SerializeField]
+    private float _bobAmount;
+
+    [SerializeField]
+    private float _bobSpeed;
+
+    private float _bobPhase;
+
     private LevelManager _levelManager;
     private bool _isCollected;
+
+    private void Update()
+    {
+        _bobPhase += Time.deltaTime * _bobSpeed;
+        
+        transform.Rotate(new Vector3(0, _spinSpeed * Time.deltaTime, 0));
+        _childTransform.localPosition = new Vector3(0, Mathf.Sin(_bobPhase) * _bobAmount, 0);
+    }
 
     private void Start()
     {
--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Level complete screen
 Sounds
 Music
 
-model for the collectible
 make the logs keep rolling after falling
 
 stop player velocity when winning
 stop doing animation events once win kicks in
+water pipes water falling
